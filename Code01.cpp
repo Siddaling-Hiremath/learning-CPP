25677b9672664f2357f822407cb2e6b7c9cb0ecb
Siddaling Hiremath <siddalinghiremath19@gmail.com>
2025-10-26 00:15:19 +0530

    WELL come

---
+++ Code01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char name[15];
+
+    cout<<"Enter your name :"<<endl;
+    cin>>name;
+
+    cout<<"Hi "<<name<<" well come to coding World ";
+
+    return 0;
+}
