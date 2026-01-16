28df6ed5ccc61524dfea522c77d2911a122d4914
Siddaling Hiremath <siddalinghiremath19@gmail.com>
2026-01-16 12:52:39 +0530

    all subarray

---
+++ subarray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int n=5,arr[5]={1,4,3,5,6};
+
+    for(int st=0;st<n;st++){
+        for(int end=st;end<n;end++){
+            for(int i=st;i<=end;i++){
+            cout<<arr[i];
+            }
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
